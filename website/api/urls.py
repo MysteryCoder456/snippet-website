2fafee359080a847728ab3a3027b450d046f67fe
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-01-13 19:08:04 +0400

    updated project structure

--- website/api/urls.py
+++ website/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
 from api import views
 
-
-urlpatterns = [path("", views.home)]
+urlpatterns = [path("", views.home, name="home")]
--- website/api/views.py
+++ website/api/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello There!")
+    return HttpResponse("<h1>Hello There</h1>")
