d0681399304ec4101b986cb4afeac1045116bde4
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-03-20 19:19:39 +0400

    rocket boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{launch, routes};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
