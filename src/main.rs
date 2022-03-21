1d7f172aa60a370cf7aaf2e7657fd14c2bd9fd7c
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-03-21 13:22:00 +0400

    added sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use rocket::{launch, routes};
+use rocket::{routes, State};
+use sqlx::postgres::PgPool;
 
 #[macro_use]
 extern crate rocket;
 
+struct DBState {
+    pool: PgPool,
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index(db_state: &State<DBState>) -> String {
+    let pool = &db_state.pool;
+    let snippets = sqlx::query!("SELECT * FROM code_snippets")
+        .fetch_all(pool)
+        .await
+        .unwrap();
+
+    snippets
+        .iter()
+        .map(|snippet| format!("{}", snippet.title))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    dotenv::dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+    let pool = PgPool::connect(db_url.as_str()).await.unwrap();
+
+    rocket::build()
+        .manage(DBState { pool })
+        .mount("/", routes![index])
 }
